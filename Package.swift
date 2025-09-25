f7e7aaff5312975beaba89b596f40cde7e5ad4bb
Oleg Morajko <olegmorajko@gmail.com>
2025-09-25 16:59:16 +0200

    Bug fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
       name: "MocaSDKBinary",
       url: "https://bin.mocaplatform.com/releases/moca-ios-sdk-3.11.0.xcframework.zip",
       checksum: "3d66191ef85e5a8c80bdc5b29823736dc52c7df90673522c33df3568c79c9fb3"
-    )
+    ),
     // Wrapper target that applies the same link requirements CocoaPods did
     .target(
       name: "MocaSDK",
